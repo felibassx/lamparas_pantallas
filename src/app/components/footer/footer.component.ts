4917499f331f5ee9a64f5d1ba9ef0e35018d0e5b
felibasscl <felibass@gmail.com>
2017-09-10 21:15:34 -0300

    header, Body y Footer listos

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: []
+})
+
+export class FooterComponent {
+
+  anio:number =  new Date().getFullYear();
+  email:string = "kafecode@gmail.com";
+
+  constructor() { }
+
+}
