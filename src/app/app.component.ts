2ead061ef1050e6e3760245bd3420eaf928c3f86
felibasscl <felibass@gmail.com>
2017-09-10 22:51:34 -0300

    servicio de informacion para las paginas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+//importar servicios
+import { InformacionService } from "./services/informacion/informacion.service";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(public _is:InformacionService){
+    
+  }
 }
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InformacionService } from "../../services/informacion/informacion.service";
 
 @Component({
   selector: 'app-footer',
@@ -9,8 +10,8 @@ import { Component } from '@angular/core';
 export class FooterComponent {
 
   anio:number =  new Date().getFullYear();
-  email:string = "kafecode@gmail.com";
 
-  constructor() { }
+  constructor(public _is:InformacionService) {
+  }
 
 }
